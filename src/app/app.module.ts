abc1f9940539e95bab49780ba505ad63cebb8382
“Anthony <anthonyjmcbride@icloud.com>
2017-07-31 02:33:23 -0400

    Message Board App - Front End Esatblished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from '@angular/material';
+import { MessagesComponent } from './messages-component'
 import { AppComponent }  from './app.component';
+import { MdButtonModule, MdCheckboxModule, MdCardModule } from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, MaterialModule, MdButtonModule, MdCheckboxModule, MdCardModule ],
+  declarations: [ AppComponent, MessagesComponent ],
+  bootstrap:    [ AppComponent ],
+
 })
 export class AppModule { }
---
+++ src/app/messages-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'messages',
+  template:`
+  <div *ngFor="let message of messages">
+    <md-card style="margin:8px">
+      <md-card-title>{{message.owner}}</md-card-title>
+      <md-card-content>{{message.text}}</md-card-content>
+    </md-card>
+  </div>`
+})
+export class MessagesComponent {
+  messages = [{text: 'some random text', owner: 'Anthony'}, {text: 'other random text', owner: 'John'}];
+}
